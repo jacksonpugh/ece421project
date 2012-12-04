09df21ee6578e899f1107fcb1a1ba4c639e9e184
jackson <japugh@pdx.edu>
2012-12-04 03:37:47 -0800

    Updated resistor v1 matlab + ltspice values

--- Matlab/MOS_Resistor/MOS_Res.m
+++ Matlab/MOS_Resistor/MOS_Res.m
@@ -9,8 +9,13 @@ Vtn = .45;
 unCox = 270e-6;
 upCox = 70e-6;
 lambdaL = .08e-6;
-
+P = 500*10^-6;
+Id = P/Vdd
+Vref = 1.24;
 B = sqrt((1/4)*(unCox/upCox));
-
+WL = 2*Id/(unCox*(Vref-Vtn)^2) % W/L ratio
+Kn = unCox*WL
 TC_Vtn = -.003;
 TC_Vtp = -.003;
+
+R = (((-(Vref - Vtn))^2)^-1)*((2*(Vdd-Vref))/Kn)
